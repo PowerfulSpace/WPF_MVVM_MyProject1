b3529f1f2f441b31d3944e19337fce879bcde67c
Aliaksandr Kudrautsau <83361252+PowerfulSpace@users.noreply.github.com>
2021-10-14 19:09:28 +0300

    Базовая логика модели представления статистики

--- WpfApp4/ViewModels/CountryStaticViewModel.cs
+++ WpfApp4/ViewModels/CountryStaticViewModel.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfApp4.Infrastructure.Commands;
+using WpfApp4.Models;
 using WpfApp4.Services;
 using WpfApp4.ViewModels.Base;
 
@@ -12,13 +15,58 @@ namespace WpfApp4.ViewModels
     {
         private DataService _DataService;
         private MainWindowViewModel MainModel { get; }
+
+
+
+
+        #region Команды
+
+        public ICommand RefreshDataCommand { get; }
+
+        private void OnRefreshDataCommandExecuted(object p)
+        {
+            Countries = _DataService.GetData();
+        }
+
+        #endregion
+
+
+
+
+
         public CountryStaticViewModel(MainWindowViewModel MainModel)
         {
             this.MainModel = MainModel;
-
             _DataService = new DataService();
 
+            RefreshDataCommand = new LambdaCommand(OnRefreshDataCommandExecuted);
         }
+
+
+
+
+
+
+
+        #region Свойства
+
+
+
+        #region Countries Статистика по странам
+
+        private IEnumerable<CountryInfo> _Countries;
+
+        public IEnumerable<CountryInfo> Countries
+        {
+            get => _Countries; 
+            private set => Set(ref _Countries, value);
+        }
+
+        #endregion
+
+
+        #endregion
+
 
     }
 }
