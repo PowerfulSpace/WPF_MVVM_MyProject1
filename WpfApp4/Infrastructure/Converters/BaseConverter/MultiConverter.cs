f49cc467b689c927b5478d9529cfaa8f17db2f31
Aliaksandr Kudrautsau <83361252+PowerfulSpace@users.noreply.github.com>
2021-10-16 19:30:02 +0300

    Добавили мультиконвертер

---
+++ WpfApp4/Infrastructure/Converters/BaseConverter/MultiConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfApp4.Infrastructure.Converters.BaseConverter
+{
+
+    internal abstract class MultiConverter : IMultiValueConverter
+    {
+        public abstract object Convert(object[] values, Type targetType, object parameter, CultureInfo culture);
+
+        public virtual object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            => throw new NotSupportedException("Обратное преобразование не поддерживается");
+
+    }
+
+}
---
+++ WpfApp4/Infrastructure/Converters/ToArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using WpfApp4.Infrastructure.Converters.BaseConverter;
+
+namespace WpfApp4.Infrastructure.Converters
+{
+    class ToArray : MultiConverter
+    {
+
+        public override object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var collection = new CompositeCollection();
+            foreach (var value in values)
+            {
+                collection.Add(value);
+            }
+            return collection;
+        }
+
+        //public override object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) => value as object[];
+
+
+    }
+}
