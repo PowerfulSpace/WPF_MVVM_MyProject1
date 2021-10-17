bec4b1c0d319a56e08822a27a8be33e06bc5493a
Aliaksandr Kudrautsau <83361252+PowerfulSpace@users.noreply.github.com>
2021-10-17 14:31:03 +0300

    Не отработал  oxy Plot. Соответственно не получилось сделать конвертер point Location, Скорее всего из-за того что это пишеться на framework а не на core

---
+++ WpfApp4/Infrastructure/Converters/PointToMapLocation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+using WpfApp4.Infrastructure.Converters.BaseConverter;
+
+
+namespace WpfApp4.Infrastructure.Converters
+{
+    [MarkupExtensionReturnType(typeof(PointToMapLocation))]
+    [ValueConversion(typeof(Point), typeof(Location))]
+    internal class PointToMapLocation /*: Converter*/
+    {
+        //public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        //{
+        //    if (!(value is Point point)) return null;
+        //    return new Location(point.X, point.Y);
+        //}
+
+        //public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        //{
+        //    if (!(value is Location location)) return null;
+        //    return new Point(location.Latitude, location.Latitude);
+        //}
+
+    }
+}
+
+// 4 урок. время: 3 35 20
